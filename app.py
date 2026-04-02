4e496277a49dfd145c06f32d94a96624460d0588
sonukumar32977 <sonukumar32977@gmail.com>
2026-04-02 14:57:32 +0530

    Updated project

--- app.py
+++ app.py
@@ -13,8 +13,9 @@ st.write("""This app is for getting a price estimation for the customer so a car
 age = st.number_input("Enter the age", min_value=18, max_value=90, value=40, step=1)
 salary= st.number_input("Enter the salary", min_value=1000, max_value=99999999, step=5000, value=30000)
 networth= st.number_input("Enter the networth",min_value=0, max_value=9999999, step= 20000, value=100000)
+credit_card_debt= st.number_input("Enter the credit card debt",min_value=0, max_value=9999999, step= 1000, value=5000)
 
-X= [age,salary,networth]
+X= [age,salary,networth,credit_card_debt]
 
 calculatebutton = st.button("Calculate")
 st.divider()
